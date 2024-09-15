1d9bfe9f89211ce9c2da8149de6111cc51e61b8e
somlyaip <somlyai.pal@gmail.com>
2024-09-15 18:39:33 +0200

    chore: fix bootstrap array declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -18,9 +17,7 @@ import { ReactiveFormsModule } from '@angular/forms';
       echarts: () => import('echarts'),
     }),
     ReactiveFormsModule,
-    AppComponent,
   ],
   providers: [],
-  bootstrap: [AppComponent],
 })
 export class AppModule {}
